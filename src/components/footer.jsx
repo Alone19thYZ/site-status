8d01dff06345835b21347dce65549f9bcca4fd12
何辰风 <2583130081@qq.com>
2024-06-20 14:38:28 +0800

    更新 footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -36,7 +36,7 @@ const Footer = () => {
         <p>
           Copyright&nbsp;&copy;&nbsp;2020&nbsp;-&nbsp;{new Date().getFullYear()}
           &nbsp;
-          <CustomLink to="https://www.chengxiaoyu.top/" text="何辰风" />
+          <CustomLink to="https://www.chengxiaoyu.top/" text="Alone19thYZ" />
           {siteIcp ? (
             <React.Fragment>
               &nbsp;|&nbsp;
